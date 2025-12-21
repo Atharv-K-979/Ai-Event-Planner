79194e9cf0bc303cfb150cf788a30b55844a2517
Atharv Kulakrni <atharvkulkarni979@gmail.com>
2025-12-21 15:06:55 +0530

    Some edits

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,23 +5,23 @@ const Footer = () => {
   return (
     <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
       <div className="flex items-center gap-6 text-sm text-gray-400">
-        Made with ❤️ by RoadsideCoder
+        Made with ❤️ by Atharv K
       </div>
 
       <div className="flex gap-4 items-center">
         <a
-          href="https://www.youtube.com/roadsidecoder"
+          href="https://youtube.com/@atharvk853?si=b9AT2efKEkXjvh0n"
           className="text-gray-400"
         >
           <Youtube />
         </a>
         <a
-          href="https://www.instagram.com/roadsidecoder"
+          href="https://www.instagram.com/atharv_k_656?igsh=MWNhN3dtaDRwN3RmMA=="
           className="text-gray-400"
         >
           <Instagram className="w-5 h-5" />
         </a>
-        <a href="https://x.com/Piyush_eon" className="text-gray-400">
+        <a href="https://x.com/atharv_k_853" className="text-gray-400">
           <Twitter className="w-5 h-5" />
         </a>
       </div>
